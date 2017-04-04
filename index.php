b0a8f75ca482ef72adbab7d2aa33bc4c5a4b442a
benit3s <gabriel.benites@hotmail.com>
2017-04-04 17:45:23 -0300

    file
    description

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+	
+	private $nome;
+	private $idade;
+	
+	public function __construct($nome, $idade){
+		$this->nome = $nome;
+		$this->idade = $idade;
+	}
+	public function getNome(){
+		return $this->nome;
+	}
+	public function getIdade(){
+		return $this->idade;
+	}	
+}
+class PessoaAdapter {
+	private $sexo;
+	private $pessoa;
+	
+	public function __construct(Pessoa $pessoa){
+		$this->pessoa = $pessoa;
+	}
+	
+	public function setSexo($s){
+		$this->sexo = $s;
+	}
+	public function getsexo(){
+		return $this->sexo;
+	}
+}
+?>
